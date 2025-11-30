b95c51d77a5be9e4cfc3365dc6e193b43b417802
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-11-30 17:42:33 -0300

    feat:  connect mongoose

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ const routes = require("./routes/order.routes");
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/ordersdb")
+  .then(() => console.log("MongoDB conectado"))
+  .catch((err) => console.error("Erro ao conectar no Mongo:", err));
+
+app.use("/", routes);
+
+app.listen(3000, () => {
+  console.log("API rodando em http://localhost:3000");
+});
